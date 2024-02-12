11668463608c3ea02c64f3939fa48b9ec64b9a6f
hakjun <hakie2kim@gmail.com>
2024-02-12 20:43:49 +0900

    feat: 적립금 적립 내역

---
+++ src/main/java/com/brokurly/repository/PointDao.java
@@ -0,0 +1,17 @@
+package com.brokurly.repository;
+
+import com.brokurly.entity.PointLogExpDto;
+import com.brokurly.entity.PointLogUsageDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface PointLogDao {
+    List<PointLogUsageDto> selectUsageByCustomerAndPeriod(@Param("custId") String custId, @Param("period") Integer period);
+    int countUsageByCustomerAndPeriod(@Param("custId") String custId, @Param("period") Integer period);
+    List<PointLogExpDto> selectExpByCustomerAndPeriod(@Param("custId") String custId, @Param("period") Integer period);
+    int countExpByCustomerAndPeriod(@Param("custId") String custId, @Param("period") Integer period);
+    List<PointLogUsageDto> selectEarningByCustomerAndPeriod(@Param("custId") String custId, @Param("period") Integer period);
+}
---
+++ src/main/java/com/brokurly/service/PointService.java
@@ -0,0 +1,35 @@
+package com.brokurly.service;
+
+import com.brokurly.entity.PointLogExpDto;
+import com.brokurly.entity.PointLogUsageDto;
+import com.brokurly.repository.PointLogDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PointLogService {
+    private final PointLogDao pointLogDao;
+
+    public List<PointLogUsageDto> findPointLogUsageByCustomerAndPeriod(String custId, Integer period) {
+        return pointLogDao.selectUsageByCustomerAndPeriod(custId, period);
+    }
+
+    public int getPointLogUsageCountByCustomerAndPeriod(String custId, Integer period) {
+        return pointLogDao.countUsageByCustomerAndPeriod(custId, period);
+    }
+
+    public List<PointLogExpDto> findPointLogExpByCustomerAndPeriod(String custId, Integer period) {
+        return pointLogDao.selectExpByCustomerAndPeriod(custId, period);
+    }
+
+    public int getPointLogExpCountByCustomerAndPeriod(String custId, Integer period) {
+        return pointLogDao.countExpByCustomerAndPeriod(custId, period);
+    }
+
+    public List<PointLogUsageDto> findPointLogEarningByCustomerAndPeriod(String custId, Integer period) {
+        return pointLogDao.selectEarningByCustomerAndPeriod(custId, period);
+    }
+}
