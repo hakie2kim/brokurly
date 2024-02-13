dea28657cfc77331f09a67d41b5103f082c61a56
psora <72380692+cbzkcbzk7@users.noreply.github.com>
2024-02-13 16:23:05 +0900

    feat:front-end 정규식

--- src/main/java/com/brokurly/controller/MemberController.java
+++ src/main/java/com/brokurly/controller/MemberController.java
@@ -2,17 +2,12 @@ package com.brokurly.controller;
 
 import com.brokurly.domain.MemberAndSignup;
 import com.brokurly.service.MemberService;
-
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
