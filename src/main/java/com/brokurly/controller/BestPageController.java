97b41c3f57f6ab6b8e96023347692bfafcf68fbc
nahye <nnhh.shine@gmail.com>
2024-02-13 19:09:17 +0900

    수정

--- src/main/java/com/brokurly/controller/BestPageController.java
+++ src/main/java/com/brokurly/controller/BestPageController.java
@@ -2,21 +2,24 @@ package com.brokurly.controller;
 
 import com.brokurly.dto.CategoryDto;
 import com.brokurly.service.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/categories")
 public class BestPageController {
 
-    @Autowired
-    CategoryService categoryService;
+    private final CategoryService categoryService;
 
     @GetMapping("/header")
     public String readCategory(Model m) throws Exception {
@@ -38,6 +41,20 @@ public class BestPageController {
         m.addAttribute("selectMain", selectMain);
 
         return "categories/best-page";
+    }
+
+    @RequestMapping(value = "/{codeId}", method = RequestMethod.GET)
+    public String categoryPage(Model m, @PathVariable int codeId) throws Exception{
+
+
+        List<CategoryDto> selectMain = categoryService.readMain();
+
+
+        m.addAttribute("codeId",codeId);
+
+
+        return "categories";
+
     }
 
 }
