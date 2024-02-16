3d6cab84b13e629cf9c1d42630d8207282763521
aaetom <omniboy13sl@gmail.com>
2024-02-16 18:30:56 +0900

    Fix : Domain으로 수정

---
+++ src/main/java/com/brokurly/domain/FAQCate.java
@@ -0,0 +1,12 @@
+package com.brokurly.domain;
+
+import lombok.*;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class FAQCate {
+    private String code;
+    private String name;
+}
--- src/main/java/com/brokurly/service/FAQService.java
+++ src/main/java/com/brokurly/service/FAQService.java
@@ -1,20 +1,27 @@
 package com.brokurly.service;
 
-import com.brokurly.entity.FAQListDto;
-import com.brokurly.repository.FAQDao;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.brokurly.domain.FAQBoard;
+import com.brokurly.domain.FAQCate;
+import com.brokurly.dto.FAQListDto;
+import com.brokurly.repository.FAQCateDao;
+import com.brokurly.repository.FAQBoardDao;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class FAQService {
-    @Autowired
-    FAQDao faqDao;
+    private final FAQBoardDao faqBoardDao;
+    private final FAQCateDao faqCateDao;
+
 
     public List<FAQListDto> getFaqList() {
-        return faqDao.selectFAQList();
+        List<FAQBoard> faqBoardList =  faqBoardDao.selectFAQList();
+        List<FAQCate> faqCateList =  faqCateDao.selectFAQCateList();
+        return new FAQBoard().pleasejoin(faqBoardList, faqCateList);
     }
-
-
 }
